40fab2667e57f1216b9a2d377a1d52c6689919c4
Senevirathna12 <anishantha702@gmail.com>
2025-11-07 17:55:29 +0530

    completed add user post request

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,10 @@ public class UserController {
     @GetMapping("/getusers")
     public List<UserDTO> getUsers(){
         return userService.getAllusers();
+    }
+
+    @PostMapping("/createuser")
+    public UserDTO addUser(@RequestBody UserDTO userDTO){
+        return userService.addUser(userDTO);
     }
 }
