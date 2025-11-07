4b69275ab684b735f30f84daca64051f8e5f0594
Senevirathna12 <anishantha702@gmail.com>
2025-11-07 13:51:03 +0530

    build actual folder structure controller, dto ,model, repo,service and complete get request mapping

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllusers();
+    }
+}
